c5c820689b19a665b8368f9b9f0d9bab5715ee4a
skyoo2003 <skyoo2003@gmail.com>
2019-05-06 22:27:40 +0900

    MySQL 도커 컨테이너 이름 수정

--- mysql-pack/run.sh
+++ mysql-pack/run.sh
@@ -5,13 +5,13 @@ LOCALREPO="$PWD/repo"
 
 git clone "$REPOSRC" "$LOCALREPO" 2> /dev/null || (cd "$LOCALREPO" && git pull)
 
-CONTAINER_ID=$(docker ps -f name=rental_games -q)
+CONTAINER_ID=$(docker ps -f name=rental_games_mysql -q)
 if [ ! -z $CONTAINER_ID ]; then
     docker kill $CONTAINER_ID
 fi
 
 docker run -d --rm -p 3306:3306 \
-    --name rental_games \
+    --name rental_games_mysql \
     -e MYSQL_DATABASE=rental_games \
     -e MYSQL_ALLOW_EMPTY_PASSWORD=true \
     -v $LOCALREPO/tables:/docker-entrypoint-initdb.d:ro \
